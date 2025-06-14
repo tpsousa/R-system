fa60d825d347b492a075c0c8e8967525999abe48
Thiago Sousa <155773063+tpsousa@users.noreply.github.com>
2025-06-14 10:34:50 -0300

    R-system

---
+++ script.js
@@ -0,0 +1,52 @@
+const dados = {
+  estruturas: [
+    { nome: "Daniel Raposo", pontuacao: 100, foto: "https://avatars.dicebear.com/api/avataaars/fox.svg" },
+    { nome: "Victor Hugo", pontuacao: 96, foto: "https://avatars.dicebear.com/api/avataaars/owl.svg" },
+    { nome: "Yann Lucas", pontuacao: 80, foto: "https://avatars.dicebear.com/api/avataaars/deer.svg" },
+    { nome: "Andressa Gabriele", pontuacao: 79, foto: "https://avatars.dicebear.com/api/avataaars/cat.svg" },
+    { nome: "Rafael", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/penguin.svg" },
+    { nome: "Sophia", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/rabbit.svg" },
+    { nome: "Geovanne", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/dog.svg" },
+  ],
+  git: [
+    { nome: "Daniel Raposo", pontuacao: 96, foto: "https://avatars.dicebear.com/api/avataaars/fox.svg" },
+    { nome: "Victor Hugo", pontuacao: 90, foto: "https://avatars.dicebear.com/api/avataaars/owl.svg" },
+    { nome: "Rafael", pontuacao: 82, foto: "https://avatars.dicebear.com/api/avataaars/penguin.svg" },
+    { nome: "Andressa Gabriele", pontuacao: 81, foto: "https://avatars.dicebear.com/api/avataaars/cat.svg" },
+    { nome: "Yan", pontuacao: 77, foto: "https://avatars.dicebear.com/api/avataaars/deer.svg" },
+    { nome: "Geovanne", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/dog.svg" },
+    { nome: "Sophia", pontuacao: 76, foto: "https://avatars.dicebear.com/api/avataaars/rabbit.svg" },
+  ],
+  html: [
+    { nome: "Victor Hugo", pontuacao: 100, foto: "https://avatars.dicebear.com/api/avataaars/owl.svg" },
+    { nome: "Elias", pontuacao: 90, foto: "https://avatars.dicebear.com/api/avataaars/fox.svg" },
+    { nome: "Rafael", pontuacao: 88, foto: "https://avatars.dicebear.com/api/avataaars/penguin.svg" },
+    { nome: "Daniel", pontuacao: 87, foto: "https://avatars.dicebear.com/api/avataaars/fox.svg" },
+    { nome: "Sophia", pontuacao: 85, foto: "https://avatars.dicebear.com/api/avataaars/rabbit.svg" },
+    { nome: "Geovanne", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/dog.svg" },
+    { nome: "Andressa", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/cat.svg" },
+    { nome: "Yan Lucas", pontuacao: 75, foto: "https://avatars.dicebear.com/api/avataaars/deer.svg" },
+  ]
+};
+
+
+function renderRanking(idElemento, listaAlunos) {
+  const ul = document.getElementById(idElemento);
+  listaAlunos
+    .sort((a, b) => b.pontuacao - a.pontuacao)
+    .forEach((aluno, index) => {
+      const li = document.createElement("li");
+      li.classList.add("ranking-item");
+      li.innerHTML = `
+        <span class="posicao">#${index + 1}</span>
+        <img src="${aluno.foto}" alt="Ícone de ${aluno.nome}" />
+        <span class="nome">${aluno.nome}</span>
+        <span class="pontuacao">${aluno.pontuacao} pts</span>
+      `;
+      ul.appendChild(li);
+    });
+}
+
+renderRanking("ranking-estruturas", dados.estruturas);
+renderRanking("ranking-git", dados.git);
+renderRanking("ranking-html", dados.html);
